79856baf6fa42ca6f982bc66ab5ced30700cbb2c
faudz02 <bthai8664@gmail.com>
2026-07-27 12:32:59 +0700

    Update FakePlayerPacket.java

--- FakePlayerPacket.java
+++ FakePlayerPacket.java
@@ -1,160 +1,146 @@
 package com.sentio.fakeplayer;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.events.PacketContainer;
-
-import com.comphenix.protocol.wrappers.PlayerInfoData;
-import com.comphenix.protocol.wrappers.EnumWrappers;
-
-import com.mojang.authlib.GameProfile;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class FakePlayerPacket {
 
 
-    private static final ProtocolManager manager =
-            ProtocolLibrary.getProtocolManager();
+    private static final Set<String> fakeTabPlayers =
+            new HashSet<>();
 
 
 
-    // Thêm Fake Player vào TAB
+
     public static void addFakePlayer(String name) {
 
 
-        UUID uuid = UUID.randomUUID();
+        if (fakeTabPlayers.contains(name)) {
+            return;
+        }
 
 
-        GameProfile profile =
-                new GameProfile(uuid, name);
-
-
-
-        PacketContainer packet =
-                manager.createPacket(
-                        PacketType.Play.Server.PLAYER_INFO
-                );
+        fakeTabPlayers.add(name);
 
 
 
-        List<PlayerInfoData> data =
-                new ArrayList<>();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+
+            sendAdd(player, name);
+
+        }
 
 
-        PlayerInfoData info =
-                new PlayerInfoData(
-                        profile,
-                        50,
-                        EnumWrappers.NativeGameMode.SURVIVAL,
-                        null
-                );
-
-
-        data.add(info);
-
-
-
-        packet.getPlayerInfoAction()
-                .write(0,
-                EnumWrappers.PlayerInfoAction.ADD_PLAYER);
-
-
-
-        packet.getPlayerInfoDataLists()
-                .write(0, data);
-
-
-
-        send(packet);
-
+        Bukkit.getLogger().info(
+                "[SentioFakePlayer] Added TAB: " + name
+        );
 
     }
 
 
 
 
-    // Xóa Fake Player khỏi TAB
+
     public static void removeFakePlayer(String name) {
 
 
-        PacketContainer packet =
-                manager.createPacket(
-                        PacketType.Play.Server.PLAYER_INFO
-                );
-
-
-        GameProfile profile =
-                new GameProfile(
-                        UUID.randomUUID(),
-                        name
-                );
+        fakeTabPlayers.remove(name);
 
 
 
-        List<PlayerInfoData> data =
-                new ArrayList<>();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+
+            sendRemove(player, name);
+
+        }
 
 
-        data.add(
-                new PlayerInfoData(
-                        profile,
-                        0,
-                        EnumWrappers.NativeGameMode.SURVIVAL,
-                        null
-                )
+        Bukkit.getLogger().info(
+                "[SentioFakePlayer] Removed TAB: " + name
         );
 
-
-
-        packet.getPlayerInfoAction()
-                .write(
-                    0,
-                    EnumWrappers.PlayerInfoAction.REMOVE_PLAYER
-                );
+    }
 
 
 
-        packet.getPlayerInfoDataLists()
-                .write(0, data);
 
 
+    private static void sendAdd(
+            Player receiver,
+            String name
+    ) {
 
-        send(packet);
+
+        /*
+         * Paper 1.21.1 NMS:
+         *
+         * ClientboundPlayerInfoUpdatePacket
+         * GameProfile
+         * ServerPlayer
+         *
+         * Cần build theo mapping
+         * paperweight-userdev
+         */
+
+
+        receiver.sendMessage(
+                "§a[FakePlayer] §f" + name
+        );
 
     }
 
 
 
 
-    // Gửi packet cho toàn bộ người chơi
-    private static void send(PacketContainer packet) {
+
+    private static void sendRemove(
+            Player receiver,
+            String name
+    ) {
 
 
-        for (Player player :
-                Bukkit.getOnlinePlayers()) {
+        /*
+         * ClientboundPlayerInfoRemovePacket
+         */
 
 
-            try {
-
-                manager.sendServerPacket(
-                        player,
-                        packet
-                );
-
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
-            }
-        }
     }
-                  }
+
+
+
+
+
+    public static boolean exists(String name) {
+
+
+        return fakeTabPlayers.contains(name);
+
+    }
+
+
+
+
+
+    public static Set<String> getPlayers() {
+
+
+        return fakeTabPlayers;
+
+    }
+
+
+
+
+
+    public static void clear() {
+
+
+        fakeTabPlayers.clear();
+
+    }
+
+}
