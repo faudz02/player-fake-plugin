7b62181b083e88eaf12dc7d9f68b8f93f5b5418e
faudz02 <bthai8664@gmail.com>
2026-07-27 12:26:12 +0700

    Update SentioFakePlayer.java

--- SentioFakePlayer.java
+++ SentioFakePlayer.java
@@ -1,19 +1,95 @@
 package com.sentio.fakeplayer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class SentioFakePlayer extends JavaPlugin {
 
+
+    private static SentioFakePlayer instance;
+
+
+
     @Override
     public void onEnable() {
-        getLogger().info("SentioFakePlayer đã bật!");
+
+
+        instance = this;
+
+
+        getLogger().info(
+                "SentioFakePlayer da bat!"
+        );
+
+
+
+        // Đăng ký command
 
         getCommand("fakeplayer")
-                .setExecutor(new FakePlayerCommand());
+                .setExecutor(
+                        new FakePlayerCommand()
+                );
+
+
+
+        // Đăng ký Chat AI
+
+        Bukkit.getPluginManager()
+                .registerEvents(
+                        new FakePlayerReplyAI(),
+                        this
+                );
+
+
+
+        // Bật AI di chuyển
+
+        FakePlayerAI.start();
+
+
+
+        // Bật Chat tự động
+
+        FakePlayerChatAI.start();
+
+
+
     }
+
+
+
+
 
     @Override
     public void onDisable() {
-        getLogger().info("SentioFakePlayer đã tắt!");
+
+
+        getLogger().info(
+                "SentioFakePlayer da tat!"
+        );
+
+
+        // Xóa dữ liệu khi tắt
+
+        FakePlayerEntity.clear();
+
+
+        FakePlayerAI.stop();
+
+
+        FakePlayerChatAI.stop();
+
     }
+
+
+
+
+
+    public static SentioFakePlayer getInstance() {
+
+        return instance;
+
+    }
+
 }
