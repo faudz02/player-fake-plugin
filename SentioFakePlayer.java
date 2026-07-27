6dd3253b99a64292274f9ebe9958d9ff761a48d7
faudz02 <bthai8664@gmail.com>
2026-07-27 11:52:24 +0700

    Update SentioFakePlayer.java

--- SentioFakePlayer.java
+++ SentioFakePlayer.java
@@ -1,1 +1,19 @@
+package com.sentio.fakeplayer;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SentioFakePlayer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("SentioFakePlayer đã bật!");
+
+        getCommand("fakeplayer")
+                .setExecutor(new FakePlayerCommand());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SentioFakePlayer đã tắt!");
+    }
+}
